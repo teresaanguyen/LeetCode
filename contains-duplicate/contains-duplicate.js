938b363a53fb84c37f3c3b6d166ecf295b6e64ad
Teresa Nguyen <88606876+teresaanguyen@users.noreply.github.com>
2022-01-07 01:03:16 -0800

    Time: 2211 ms, Memory: 46.2 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums.indexOf(nums[i], i + 1) !== -1) {
+            return true;
+        }
+    }
+    return false;
+};
