55d356f14e6e7c8c238feff92507877df11b2723
Teresa Nguyen <88606876+teresaanguyen@users.noreply.github.com>
2022-01-20 21:33:35 -0800

    Time: 84 ms, Memory: 40.6 MB - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -4,14 +4,19 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // iterate through array using two loops
+    let obj = {};
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target && i !== j) {
-                return [i, j];
-            }
+        // find difference of target and element
+        const diff = target - nums[i];
+        // check objec to see if difference is present
+        // if not present
+        if (obj[diff] === undefined) {
+            // add to object
+            obj[nums[i]] = i;
+        // if present
+        } else {
+            // return i and value of different pair in obj
+            return [i, obj[diff]]
         }
     }
-    // if sums equal to target
-    // return indexes
 };
